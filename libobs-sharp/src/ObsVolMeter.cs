46463f6b6df903a9233da0a7092831c111b00a46
sethcarlton <sethrcarlton@gmail.com>
2017-01-26 11:26:02 -0600

    Add controls for audio meter

---
+++ libobs-sharp/src/ObsVolMeter.cs
@@ -0,0 +1,88 @@
+﻿/***************************************************************************
+	Copyright (C) 2014-2015 by Ari Vuollet <ari.vuollet@kapsi.fi>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using OBS.Utility.Tuple;
+
+namespace OBS
+{
+	using delegateTuple = Tuple<libobs.obs_volmeter_updated_t, IntPtr>;
+
+	public class ObsVolMeter : IObsContextData, IDisposable
+	{
+		internal IntPtr instance; //pointer to unmanaged object
+
+		// stored references to all delegates
+		private List<delegateTuple> delegateRefs = new List<delegateTuple>();
+
+		public unsafe ObsVolMeter(ObsFaderType faderType)
+		{
+			instance = libobs.obs_volmeter_create((libobs.obs_fader_type)faderType);
+
+			if (instance == null)
+				throw new ApplicationException("obs_volmeter_create failed");
+		}
+
+		public unsafe bool AttachSource(ObsSource source)
+		{
+			return libobs.obs_volmeter_attach_source(instance, source.GetPointer());
+		}
+
+		public unsafe void DetachSource()
+		{
+			libobs.obs_volmeter_detach_source(instance);
+		}
+
+		public unsafe void AddCallBbck(libobs.obs_volmeter_updated_t callback)
+		{
+			AddCallback(callback, IntPtr.Zero);
+		}
+
+		public unsafe void AddCallback(libobs.obs_volmeter_updated_t callback, IntPtr param)
+		{
+			// store the delegate tuple to prevent delegate getting removed
+			// by garbage collector
+
+			delegateTuple tuple = new delegateTuple(callback, param);
+			delegateRefs.Add(tuple);
+			libobs.obs_volmeter_add_callback(instance, tuple.Item1, tuple.Item2);
+		}
+
+		public unsafe void Dispose()
+		{
+			if (instance == IntPtr.Zero)
+				return;
+
+			libobs.obs_volmeter_destroy(instance);
+
+			instance = IntPtr.Zero;
+		}
+
+		public unsafe IntPtr GetPointer()
+		{
+			return instance;
+		}
+	}
+
+	public enum ObsFaderType : int
+	{
+		OBS_FADER_CUBIC,
+		OBS_FADER_IEC,
+		OBS_FADER_LOG
+	};
+}
---
+++ libobs-sharp/src/libobs/libobs/audio-controls.cs
@@ -0,0 +1,56 @@
+/***************************************************************************
+	Copyright (C) 2014-2015 by Ari Vuollet <ari.vuollet@kapsi.fi>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, see <http://www.gnu.org/licenses/>.
+***************************************************************************/
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace OBS
+{
+	using obs_volmeter_t = IntPtr;
+	using obs_source_t = IntPtr;
+
+	public static partial class libobs
+	{
+		[UnmanagedFunctionPointer(importCall, CharSet = importCharSet)]
+		public delegate void obs_volmeter_updated_t(IntPtr data, float level, float mag,
+			float peak, float muted);
+
+		[DllImport(importLibrary, CallingConvention = importCall)]
+		public static extern obs_volmeter_t obs_volmeter_create(obs_fader_type fader);
+
+		[DllImport(importLibrary, CallingConvention = importCall)]
+		public static extern void obs_volmeter_destroy(obs_volmeter_t volmeter);
+
+		[DllImport(importLibrary, CallingConvention = importCall)]
+		public static extern bool obs_volmeter_attach_source(obs_volmeter_t volmeter, obs_source_t source);
+
+		[DllImport(importLibrary, CallingConvention = importCall)]
+		public static extern void obs_volmeter_detach_source(obs_volmeter_t volmeter);
+
+		[DllImport(importLibrary, CallingConvention = importCall)]
+		public static extern void obs_volmeter_add_callback(obs_volmeter_t volmeter, obs_volmeter_updated_t callback, IntPtr param);
+
+//		EXPORT void obs_volmeter_remove_callback(obs_volmeter_t* volmeter, obs_volmeter_updated_t callback, void* param);
+
+		public enum obs_fader_type : int
+		{
+			OBS_FADER_CUBIC,
+			OBS_FADER_IEC,
+			OBS_FADER_LOG
+		};
+	}
+}
