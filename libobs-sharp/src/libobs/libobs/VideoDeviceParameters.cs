8647f71028dce7a9a57b528f92bdded644ef6b63
cluangrath <cluangrath1@gmail.com>
2019-01-17 14:08:09 -0600

    add video device parameter constants

---
+++ libobs-sharp/src/libobs/libobs/VideoDeviceParameters.cs
@@ -0,0 +1,21 @@
+﻿namespace OBS
+{
+    public static class VideoDeviceParameters
+    {
+        public const string VIDEO_DEVICE_ID = "video_device_id";
+        public const string RESOLUTION_TYPE = "res_type";
+        public const string RESOLUTION = "resolution";
+        public const string FRAME_INTERVAL = "frame_interval";
+        public const string VIDEO_FORMAT = "video_format";
+        public const string LAST_VIDEO_DEVICE_ID = "last_video_device_id";
+        public const string LAST_RESOLUTION = "last_resolution";
+        public const string BUFFERING = "buffering";
+        public const string FLIP_IMAGE = "flip_vertically";
+        public const string AUDIO_OUTPUT_MODE = "audio_output_mode";
+        public const string USE_CUSTOM_AUDIO = "use_custom_audio_device";
+        public const string AUDIO_DEVICE_ID = "audio_device_id";
+        public const string COLOR_SPACE = "color_space";
+        public const string COLOR_RANGE = "color_range";
+        public const string DEACTIVATE_WHEN_NOT_SHOWING = "deactivate_when_not_showing";
+    }
+}
