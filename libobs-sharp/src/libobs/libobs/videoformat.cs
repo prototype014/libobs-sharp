527ef66373bee9153532abb327f596c9b21a79b2
cluangrath <cluangrath1@gmail.com>
2019-01-17 11:42:20 -0600

    add audioformat and videoformat

---
+++ libobs-sharp/src/libobs/libobs/videoformat.cs
@@ -0,0 +1,28 @@
+﻿namespace libobs.src.libobs.libobs
+{
+    public enum videoformat
+    {
+        Any,
+        Unknown,
+
+        /* raw formats */
+        ARGB = 100,
+        XRGB,
+
+        /* planar YUV formats */
+        I420 = 200,
+        NV12,
+        YV12,
+        Y800,
+
+        /* packed YUV formats */
+        YVYU = 300,
+        YUY2,
+        UYVY,
+        HDYC,
+
+        /* encoded formats */
+        MJPEG = 400,
+        H264
+    }
+}
